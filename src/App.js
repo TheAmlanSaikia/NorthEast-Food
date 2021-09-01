0a1323b36d89add9f42d30d2f5c0376686e71a71
TheAmlanSaikia <83667291+TheAmlanSaikia@users.noreply.github.com>
2021-09-01 19:17:08 +0530

    Added additional information

--- src/App.js
+++ src/App.js
@@ -3,28 +3,59 @@ import React, { useState } from "react";
 
 const foodData = {
   Assamese: [
-    { name: "Chicken Curry with Bamboo Shot", rating: "4/5" },
-    { name: "Mass Tenga", rating: "3.5/5" }
+    {
+      name: "Chicken Curry with Bamboo Shot",
+      description: "Chicken boiled with shoots of bamboo.",
+      rating: "4/5"
+    },
+    {
+      name: "Mass Tenga",
+      description: "Fish cooked in tangy flavour from Tomato",
+      rating: "3.5/5"
+    },
+    {
+      name: "Aloo Pitika",
+      description: "Roasted potato meshed with onion,chilli and tomato",
+      rating: "5/5"
+    }
   ],
 
   Nagaland: [
     {
       name: "Axone",
+      description:
+        "It is a fermented soybean product commonly used in Naga cuisine.",
       rating: "5/5"
     },
     {
       name: "Pork with dry bamboo shoot",
+      description: "Smoked pork meat cooked in bamboo shot.",
+      rating: "4.5/5"
+    },
+    {
+      name: "Eel Chili Sauce",
+      description: "Dry eel pounded with lots of chilies, garlic, and salt.",
       rating: "4.5/5"
     }
   ],
   Manipur: [
     {
       name: "Chamthong",
+      description:
+        "It consists of seasonal vegetables that are boiled and flavoured.",
       rating: "3.5/5"
     },
     {
       name: "Nga Atauba Thongba",
+      description: "Fish is flavoured with crushed pepper and other spices.",
       rating: "5/5"
+    },
+
+    {
+      name: "Alu Kangmet",
+      description:
+        "Boiled potatoes are mashed wonderfully and mixed with fried red chilli, salt and drizzles of mustard oil.",
+      rating: "4.5/5"
     }
   ]
 };
@@ -52,11 +83,12 @@ export default function App() {
         ))}
       </div>
       <hr />
-      <div style={{ textAlign: "left", padding: "20px" }}>
+      <div style={{ textAlign: "left", padding: "30px" }}>
         <ul>
           {foodData[selectedState].map((food) => (
             <li key={food.name}>
               <div style={{ fontSize: "22px" }}> {food.name} </div>
+              <div style={{ fontSize: "12px" }}>{food.description}</div>
               <div style={{ fontSize: "16px" }}> {food.rating} </div>
             </li>
           ))}
